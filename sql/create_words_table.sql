11efda27e84a4826ba468307facb5920d283664d
lombardo-chcg <dpa.lombardo@gmail.com>
2017-05-27 18:38:00 -0500

    add words with friends points

--- sql/create_words_table.sql
+++ sql/create_words_table.sql
@@ -2,6 +2,7 @@ create table words (
     ID serial PRIMARY KEY,
     word varchar(256) NOT NULL,
     canonical_word varchar(256) NOT NULL,
-    points int NOT NULL,
+    scrabble_points int NOT NULL,
+    words_with_friends_points int NOT NULL,
     create_date timestamp NOT NULL default now()
 );
